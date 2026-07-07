4fd6f77f62d541e502d35494e140a3bdc8297c21
Muhammad Sami <choudharysami7264@gmail.com>
2026-07-07 18:46:52 +0500

    Refactor ASGI application setup and import routing

--- backend/nexus_backend/asgi.py
+++ backend/nexus_backend/asgi.py
@@ -1,13 +1,16 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import authentication.routing # Hum ye routing file abhi banayenge
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nexus_backend.settings')
 
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import authentication.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             authentication.routing.websocket_urlpatterns
